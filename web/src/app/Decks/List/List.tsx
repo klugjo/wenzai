901e86ad795ea6c26926d0b431fe56fddb979655
Jonathan Klughertz <klughertz.j@gmail.com>
2020-08-28 18:59:40 +0200

    buttons decks

--- web/src/app/Decks/List/List.tsx
+++ web/src/app/Decks/List/List.tsx
@@ -1,22 +1,40 @@
 import React from 'react';
+import cn from 'classnames';
+import { GiAfrica, GiCupcake, GiNoodles, GiJapan, GiMusicalNotes } from 'react-icons/gi';
+import { AiOutlinePlus, AiOutlineEdit, AiOutlineRocket, AiOutlineDelete } from 'react-icons/ai';
 
 import styles from './List.module.css';
 
 const decks = [
   {
-    name: 'Italian Pasta'
+    id: '1',
+    name: 'Italian Pasta',
+    type: 'Food',
+    icon: <GiNoodles />
   },
   {
-    name: 'Katakana - カタカナ'
+    id: '2',
+    name: 'Katakana - カタカナ',
+    type: 'Language',
+    icon: <GiJapan />
   },
   {
-    name: 'Famous operas'
+    id: '3',
+    name: 'Famous operas',
+    type: 'Music',
+    icon: <GiMusicalNotes />
   },
   {
-    name: 'African flags'
+    id: '4',
+    name: 'African flags',
+    type: 'Geography',
+    icon: <GiAfrica />
   },
   {
-    name: 'Curry spice list'
+    id: '5',
+    name: 'Curry spice list',
+    type: 'Food',
+    icon: <GiCupcake />
   }
 ];
 
@@ -25,14 +43,23 @@ interface IDeckListProps {
 }
 
 const DeckList: React.FC<IDeckListProps> = () => {
-
-
   return (
     <div className={styles.root}>
+      <div className={styles.header}>
+        <h1><span>.</span>DECKS</h1>
+        <div className={styles.newDeckButton}><AiOutlinePlus /></div>
+      </div>
       {decks.map(deck => (
-        <div className={styles.deck}>
-          <div className={styles.triangle}></div>
-          <div>{deck.name}</div>
+        <div className={styles.deck} key={deck.id}>
+          <div className={styles.icon}>{deck.icon}</div>
+          <div className={styles.titleContainer}>
+            <div className={styles.title}>{deck.name}</div>
+            <div className={styles.type}>{deck.type}</div>
+          </div>
+          <div className={styles.separator} />
+          <div className={cn(styles.button, styles.mainButton)}><AiOutlineEdit /></div>
+          <div className={styles.button}><AiOutlineRocket /></div>
+          <div className={styles.button}><AiOutlineDelete /></div>
         </div>
       ))}
     </div>
